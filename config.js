75798c29e0afcfad83c6826463654603ebbddb3a
Moretoncp <140344703+Moretoncp@users.noreply.github.com>
2026-07-29 08:27:48 -0500

    Update config.js

--- config.js
+++ config.js
@@ -16,4 +16,5 @@ const BETS = [
   { date: "2026-07-28", sport: "MLB", pick: "Phillies/Marlins o7.5", type: "Total", odds: -134, stake: 500, result: "loss", book: "" },
   { date: "2026-07-28", sport: "MLB", pick: "Padres ML vs Rockies", type: "Moneyline", odds: -223, stake: 500, result: "win", book: "" },
   { date: "2026-07-28", sport: "MLB", pick: "Padres/Rockies o7.5", type: "Total", odds: -143, stake: 500, result: "win", book: "" },
+  { date: "2026-07-29", sport: "MLB", pick: "Blue Jays vs Nationals YRFI", type: "Prop", odds: -126, stake: 250, result: "pending", book: "" },
 ];
